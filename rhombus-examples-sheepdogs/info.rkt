3cb1b4fe2fddf7d9dcd24b9ec55034e24756659e
Sam Phillips <samdphillips@gmail.com>
2021-11-01 17:33:08 -0700

    Update sheepdog example

---
+++ rhombus-examples-sheepdogs/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "rhombus-examples-sheepdogs")
+(define collection "rhombus-examples")
+(define deps
+  '("base"
+    "https://github.com/mflatt/shrubbery-rhombus-0.git"))
+(define module-suffixes '(#"rhm"))
+
