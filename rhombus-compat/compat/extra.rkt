af7920f0a161ffc286f6431c4dfe25f5c37b306c
Sam Tobin-Hochstadt <samth@indiana.edu>
2021-11-17 13:21:06 -0500

    Add extra utility macros.

---
+++ rhombus-compat/compat/extra.rkt
@@ -0,0 +1,33 @@
+#lang rhombus
+
+export:
+  |>
+  when
+  thunk
+  loop
+  letcc
+
+import: racket/base: prefix r
+        rhombus/macro: no_prefix
+                     
+expr.rule '(letcc $k: $body ...; ...):
+  '(r.#{call/cc}(fun($k): $body ...; ...))
+                
+  
+expr.rule '(loop: $body ...; ...):
+  '(begin:
+      fun lp():
+        $body ...; ...
+        lp()
+      lp())
+
+expr.rule '(thunk: $body ... ; ...):
+  '(fun (): $body ... ; ...)
+
+
+expr.rule '(when $e: $body ... ; ...):
+  '(if $e
+    | begin: $body ... ; ...
+    | r.void())
+
+operator (a |> b): b(a)
