281c6cd4556a35cd58d20de0878e0a55cd0d4a0d
Sam Phillips <samdphillips@gmail.com>
2021-11-17 16:15:33 -0800

    [compat] adjust when syntax

--- rhombus-compat/compat/extra.rkt
+++ rhombus-compat/compat/extra.rkt
@@ -9,11 +9,10 @@ export:
 
 import: racket/base: prefix r
         rhombus/macro: no_prefix
-                     
+
 expr.rule '(letcc $k: $body ...; ...):
   '(r.#{call/cc}(fun($k): $body ...; ...))
-                
-  
+
 expr.rule '(loop: $body ...; ...):
   '(begin:
       fun lp():
@@ -25,9 +24,9 @@ expr.rule '(thunk: $body ... ; ...):
   '(fun (): $body ... ; ...)
 
 
-expr.rule '(when $e: $body ... ; ...):
-  '(if $e
-    | begin: $body ... ; ...
+expr.rule '(when $e ... : $body ... ; ...):
+  '(if $e ...
+    | $body ... ; ...
     | r.void())
 
 operator (a |> b): b(a)
