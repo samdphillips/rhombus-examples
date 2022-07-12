218497a7b0ab780ba8ed0c7fee30c594ef9e5b16
Sam Phillips <samdphillips@gmail.com>
2022-07-12 10:51:22 -0700

    Fix Thread syntax

--- rhombus-compat/compat/thread.rkt
+++ rhombus-compat/compat/thread.rkt
@@ -10,12 +10,15 @@ export:
   thread
 
 annotation.macro 'Thread':
-  annotation_ct.pack_predicate('r.#{thread?}',
-                               '($(dot_ct.provider_key), thread_dot_provider)')
+  values(annotation_ct.pack_predicate('r.#{thread?}',
+                                      '(($(dot_ct.provider_key), thread_dot_provider))'),
+         '')
+
 dot.macro 'thread_dot_provider $left $dot $right':
   match right
   // One argument functions
   | 'kill': 'r.#{kill-thread}($left)'
 
-expr.rule 'thread: $body ... ; ...':
-  'r.thread(fun (): $body ... ; ...)'
+expr.rule 'thread: $body':
+  'r.thread(fun (): $body)'
+
