5a446ff2a129d02db97ab0d1b9748351029b7f5b
Sam Phillips <samdphillips@gmail.com>
2022-09-04 14:34:54 -0700

    Add echo example

---
+++ rhombus-examples-echo/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define name "rhombus-examples-echo")
+(define collection "rhombus-examples")
+(define deps
+  '("base"
+    "rhombus-prototype"
+    "rhombus-compat"))
+(define module-suffixes '(#"rhm"))
+
